f8029897a52ff77e7bda43c9d1032b6874beb6be
Sam Jordan <sjordan@gsspos.co.uk>
2022-09-02 17:14:17 +0100

    Add project files.

---
+++ popupCarouselCollectionViewHeight/popupCarouselCollectionViewHeight/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace popupCarouselCollectionViewHeight
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.ShowPopup(new MyPopup());
+        }
+    }
+}
---
+++ popupCarouselCollectionViewHeight/popupCarouselCollectionViewHeight/MyPopup.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace popupCarouselCollectionViewHeight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPopup : Popup
+    {
+        public MyPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
